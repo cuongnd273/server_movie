4e0203749f3cb1d55058ee4b387c382d8355d015
Cuong Nguyen <cuongit95@gmail.com>
2017-10-23 00:03:37 +0700

    ChangeConfigMysql

--- connect/db_config.php
+++ connect/db_config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER','sql10198024');
-define('DB_DATABASE','sql10198024');
-define('DB_PASSWORD','ZvwXShFred');
+define('DB_USER','sql3200766');
+define('DB_DATABASE','sql3200766');
+define('DB_PASSWORD','NMDyvFEQpB');
 define('DB_SERVER','sql10.freesqldatabase.com');
 ?>
